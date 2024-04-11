b9b2bd7289dc36c13abc360b6d44b8479928208e
Polimorfius <m.c.polinom@gmail.com>
2024-04-12 00:03:25 +0300

    12

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, Git!" << std::endl;
+int main()
+{
+    std::cout << "Hello, Git , бляяяаааа как же крууууто)))!" << std::endl;
+    return 0;
+}
