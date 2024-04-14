cbc95a366fa5b000b0797a61567645c7e80dcb53
Polimorfius <m.c.polinom@gmail.com>
2024-04-14 16:14:00 +0300

    test

--- main.cpp
+++ main.cpp
@@ -6,24 +6,26 @@
 #include <vector>
 using namespace std;
 
-
 int main(void)
 {
-    
+    //
+
     TimeChek tim; // обьявление начала измерения времени
-    
-   vector<string> strSity = {"Moscow","Rome","London",};
+
+    vector<string> strSity = {
+        "Moscow",
+        "Rome",
+        "London",
+    };
     tim.TimeCheks("Siti vector bild");
-   for ( auto &&strSi : strSity)
-   {
-     cout << strSi << "\n";
-    
-   }
-   tim.TimeCheks("Siti vector print");
-   
+    for (auto &&strSi : strSity)
+    {
+        cout << strSi << "\n";
+    }
+    tim.TimeCheks("Siti vector print");
 
     // инициализируем map набором пар {ключ, значение}
-    map< std::string, int> years = {
+    map<std::string, int> years = {
         {"2Moscow", 1147},
         {"2Rome", -753},
         {"2London", 47},
@@ -31,14 +33,13 @@ int main(void)
 
     tim.TimeCheks("Siti map bild");
 
-    for ( auto &&[city, year] : years)
+    for (auto &&[city, year] : years)
     {
 
-         cout << city << ": " << year << "\n";
+        cout << city << ": " << year << "\n";
         //  tim.TimeCheks(city); // завершение измерения времени
-        
     }
-tim.TimeCheks("Siti map print"); // timeChek it-3           2.875e-06
+    tim.TimeCheks("Siti map print"); // timeChek it-3           2.875e-06
 
     return 0;
 }
