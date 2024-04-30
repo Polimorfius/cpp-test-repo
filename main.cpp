d2a6e6f7519c038e5d4a8786f30291e426b94962
Polimorfius <m.c.polinom@gmail.com>
2024-04-30 16:34:41 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,45 +1,277 @@
-
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
+#include <set>
 #include <string>
-#include "TimeChek.h"
+#include <utility>
 #include <vector>
+#include <numeric>
+
 using namespace std;
 
-int main(void)
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+
+string ReadLine()
 {
-    ///
-
-    TimeChek tim; // обьявление начала измерения времени
-
-    vector<string> strSity = {
-        "Moscow",
-        "Rome",
-        "London",
-    };
-    tim.timeChek("Siti vector bild");
-    for (auto &&strSi : strSity)
-    {
-        cout << strSi << "\n";
-    }
-    tim.timeChek("Siti vector print");
-
-    // инициализируем map набором пар {ключ, значение}
-    map<std::string, int> years = {
-        {"2Moscow", 1147},
-        {"2Rome", -753},
-        {"2London", 47},
-    };
-
-    tim.timeChek("Siti map bild");
-
-    for (auto &&[city, year] : years)
-    {
-
-        cout << city << ": " << year << "\n";
-       
-    }
-    tim.timeChek("Siti map print"); 
-
-    return 0;
+    string s;
+    getline(cin, s);
+    return s;
 }
+
+int ReadLineWithNumber()
+{
+    int result;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+
+vector<string> SplitIntoWords(const string &text)
+{
+    vector<string> words;
+    string word;
+    for (const char c : text)
+    {
+        if (c == ' ')
+        {
+            if (!word.empty())
+            {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else
+        {
+            word += c;
+        }
+    }
+    if (!word.empty())
+    {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+struct Document
+{
+    int id;
+    double relevance;
+    int rating;
+};
+
+class SearchServer
+{
+public:
+    void SetStopWords(const string &text)
+    {
+        for (const string &word : SplitIntoWords(text))
+        {
+            stop_words_.insert(word);
+        }
+    }
+
+    void AddDocument(int document_id, const string &document, vector<int> rating)
+    {
+        rating_data[document_id] = ComputeAverageRating(rating);
+        ++document_count_;
+        const vector<string> words = SplitIntoWordsNoStop(document);
+        const double inv_word_count = 1.0 / words.size();
+        for (const string &word : words)
+        {
+            word_to_document_freqs_[word][document_id] += inv_word_count;
+        }
+    }
+
+    vector<Document> FindTopDocuments(const string &raw_query) const
+    {
+        const Query query = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query);
+
+        sort(matched_documents.begin(), matched_documents.end(),
+             [](const Document &lhs, const Document &rhs)
+             {
+                 return lhs.relevance > rhs.relevance;
+             });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT)
+        {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
+    }
+
+  
+
+    vector<int> InRating()
+    {
+        int in_count;
+        cin >> in_count;
+        vector<int> riting_data(in_count);
+        for (int i = 0; i < in_count; i++)
+        {
+            cin >> riting_data[i];
+        }
+        ReadLine();
+        return riting_data;
+    }
+
+private:
+    int document_count_ = 0;
+    set<string> stop_words_;
+    map<string, map<int, double>> word_to_document_freqs_;
+    map<int, int> rating_data;
+
+    static  int ComputeAverageRating(const vector<int> &ratings)
+    {
+        if (ratings.empty())
+            return 0;
+        return (accumulate(ratings.begin(), ratings.end(), 0) / ((int)ratings.size()));
+    }
+
+    bool IsStopWord(const string &word) const
+    {
+        return stop_words_.count(word) > 0;
+    }
+
+    vector<string> SplitIntoWordsNoStop(const string &text) const
+    {
+        vector<string> words;
+        for (const string &word : SplitIntoWords(text))
+        {
+            if (!IsStopWord(word))
+            {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+
+    struct QueryWord
+    {
+        string data;
+        bool is_minus;
+        bool is_stop;
+    };
+
+    QueryWord ParseQueryWord(string text) const
+    {
+        bool is_minus = false;
+        // Word shouldn't be empty
+        if (text[0] == '-')
+        {
+            is_minus = true;
+            text = text.substr(1);
+        }
+        return {text, is_minus, IsStopWord(text)};
+    }
+
+    struct Query
+    {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+
+    Query ParseQuery(const string &text) const
+    {
+        Query query;
+        for (const string &word : SplitIntoWords(text))
+        {
+            const QueryWord query_word = ParseQueryWord(word);
+            if (!query_word.is_stop)
+            {
+                if (query_word.is_minus)
+                {
+                    query.minus_words.insert(query_word.data);
+                }
+                else
+                {
+                    query.plus_words.insert(query_word.data);
+                }
+            }
+        }
+        return query;
+    }
+
+    // Existence required
+    double ComputeWordInverseDocumentFreq(const string &word) const
+    {
+        return log(document_count_ * 1.0 / word_to_document_freqs_.at(word).size());
+    }
+
+    vector<Document> FindAllDocuments(const Query &query) const
+    {
+        map<int, double> document_to_relevance;
+        for (const string &word : query.plus_words)
+        {
+            if (word_to_document_freqs_.count(word) == 0)
+            {
+                continue;
+            }
+            const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
+            for (const auto &[document_id, term_freq] : word_to_document_freqs_.at(word))
+            {
+                document_to_relevance[document_id] += term_freq * inverse_document_freq;
+            }
+        }
+
+        for (const string &word : query.minus_words)
+        {
+            if (word_to_document_freqs_.count(word) == 0)
+            {
+                continue;
+            }
+            for (const auto &[document_id, _] : word_to_document_freqs_.at(word))
+            {
+                document_to_relevance.erase(document_id);
+            }
+        }
+
+        vector<Document> matched_documents;
+        for (const auto &[document_id, relevance] : document_to_relevance)
+        {
+            matched_documents.push_back({document_id, relevance, rating_data.at(document_id)});
+        }
+        return matched_documents;
+    }
+};
+
+SearchServer CreateSearchServer()
+{
+
+    SearchServer search_server;
+    search_server.SetStopWords(ReadLine());
+
+    const int document_count = ReadLineWithNumber();
+    for (int document_id = 0; document_id < document_count; ++document_id)
+    {
+        search_server.AddDocument(document_id, ReadLine(), search_server.InRating());
+    }
+
+    return search_server;
+}
+
+int main()
+{
+    const SearchServer search_server = CreateSearchServer();
+
+    const string query = ReadLine();
+    for (auto [document_id, relevance, rating] : search_server.FindTopDocuments(query))
+    {
+        cout << "{ document_id = "s << document_id << ", "s
+             << "relevance = "s << relevance << ", "s
+             << "rating = "s << rating
+             << " }"s << endl;
+    }
+}
+/*
+и в на
+3
+белый кот и модный ошейник
+2 8 -3
+пушистый кот пушистый хвост
+3 7 2 7
+ухоженный пёс выразительные глаза
+4 5 -12 2 1
+пушистый ухоженный кот
+*/
